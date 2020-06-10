2fecd5b68715be87b691f017f40489852adbe530
Parimal Bakrola <pbakrola5@gmail.com>
2020-06-10 22:44:13 +0530

    Dark Mode

--- fresh_florida/js/script.js
+++ fresh_florida/js/script.js
@@ -1,1 +1,70 @@
-// Fixed Navbar on Scroll
+$(document).ready(function(){
+
+    // Common function for Toggles
+    (function (factory) {
+        if (typeof define === 'function' && define.amd) {
+            // AMD. Register as an anonymous module.
+            define(['jquery'], factory);
+        } else {
+            // Browser globals
+            factory(jQuery);
+        }
+    }) (function($) {
+        $.fn.funcToggle = function(type, data) {
+            var dname = "jqp_eventtoggle_" + type + (new Date()).getTime(),            
+                funcs = Array.prototype.slice.call(arguments, 2),
+                numFuncs = funcs.length,
+                empty = function() {},
+                false_handler = function() {return false;};
+    
+            if(typeof type === "object") {
+                for( var key in type) {
+                    $.fn.funcToggle.apply(this, [key].concat(type[key]));
+                }
+                return this;
+            }
+            if($.isFunction(data) || data === false) {
+                funcs = [data].concat(funcs);
+                numFuncs += 1;
+                data = undefined;
+            }
+            
+            funcs = $.map(funcs, function(func) {
+                if(func === false) {
+                    return false_handler;
+                }
+                if(!$.isFunction(func)) {
+                    return empty;
+                }
+                return func;
+            });
+    
+            this.data(dname, 0);
+            this.bind(type, data, function(event) {
+                var data = $(this).data(),
+                    index = data[dname];
+                funcs[index].call(this, event);
+                data[dname] = (index + 1) % numFuncs;
+            });
+            return this;
+        };
+    });
+
+    // Theme Toggle
+
+    let trans = () => {
+        document.documentElement.classList.add('transition');
+        window.setTimeout(() => {
+            document.documentElement.classList.remove('transition')
+        }, 1000)
+    }
+
+    $("#theme_toggle").funcToggle('click',function() {
+        trans();
+        $("html").attr("data-theme", "dark");
+        }
+        ,function(){
+            trans();
+            $("html").attr("data-theme", "light");}
+    );
+});
