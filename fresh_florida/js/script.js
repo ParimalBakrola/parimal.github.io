1bc93d5e74a1a2006fb66af53777793dcd9e2fcb
Parimal Bakrola <pbakrola5@gmail.com>
2020-06-14 22:12:23 +0530

    Phone test

--- fresh_florida/js/script.js
+++ fresh_florida/js/script.js
@@ -70,4 +70,24 @@ $(document).ready(function(){
             $("#theme_toggle span").toggleClass("icon-moon icon-sun");
         }
     );
+
+    // Plus-minus buttons Product list
+    $(document).ready(function() {
+        $('.minus').click(function () {
+            var $input = $(this).parent().find('input');
+            var count = parseInt($input.val()) - 1;
+            count = count < 1 ? 1 : count;
+            $input.val(count);
+            $input.change();
+            return false;
+        });
+        $('.plus').click(function () {
+            var $input = $(this).parent().find('input');
+            $input.val(parseInt($input.val()) + 1);
+            $input.change();
+            return false;
+        });
+    });
+
+
 });
